11d6a65dbabcf925f2e8ba21d2068eed49fa8259
Gerson Dias <gerson.dias@lambda3.com.br>
2014-11-14 19:54:19 -0200

    Align Assignment symbol

---
+++ src/CodeCracker/AssignStatementAligmentCodeFixProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace CodeCracker
+{
+    public class AssignStatementAligmentCodeFixProvider : CodeFixProvider
+    {
+        public override Task ComputeFixesAsync(CodeFixContext context)
+        {
+            return null;
+        }
+
+        public override ImmutableArray<string> GetFixableDiagnosticIds()
+        {
+            return new ImmutableArray<string>();
+        }
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return null;
+        }
+    }
+}
