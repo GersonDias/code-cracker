b95903be9e1ae90f20eca600cc91fd1f00d64912
Gerson Dias <gerson.dias@lambda3.com.br>
2014-11-15 14:37:42 -0200

    Merge MainLine -> Dev

---
+++ src/CodeCracker/AssignStatementAlignmentAnalyser.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+
+namespace CodeCracker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AssignStatementAlignmentAnalyser : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CodeCracker.AssignStatementAlignmentAnalyser";
+        internal const string Title = "Assign Statement Align";
+        internal const string MessageFormat = "{0}";
+        internal const string Category = "Syntax";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Analyser, SyntaxKind.SimpleAssignmentExpression);
+            context.RegisterSyntaxNodeAction(Analyser, SyntaxKind.VariableDeclaration);
+
+        }
+
+        private void Analyser(SyntaxNodeAnalysisContext context)
+        {
+            var semanticModel = context.SemanticModel;
+            var statement = context.Node as StatementSyntax;
+
+            var variableSymbol = semanticModel.GetSymbolInfo(statement).Symbol;
+            var assignmentExpressions = FindAssigmentExpressions(semanticModel, statement, variableSymbol);
+
+            if (assignmentExpressions.Count > 0)
+            {
+                var diagnostic = Diagnostic.Create(Rule, statement.GetLocation(), "Assign alignment.", "Align '=' symbols in assign declarations.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private List<LocalDeclarationStatementSyntax> FindAssigmentExpressions(SemanticModel semanticModel, StatementSyntax statement, ISymbol variableSymbol)
+        {
+            var blockParent = statement.FirstAncestorOrSelf<BlockSyntax>();
+
+            if (blockParent == null) return null;
+
+            var assignmentExpressions = new List<LocalDeclarationStatementSyntax>();
+
+            var indexOfStatement = blockParent.Statements.IndexOf(statement);
+
+            //Next statement is a LocalDeclaration?
+            if (blockParent.Statements[indexOfStatement + 1] is LocalDeclarationStatementSyntax)
+                return null;
+
+            for (int i = indexOfStatement; i >= 0; i--)
+            {
+
+
+
+
+
+                var blockStatement = blockParent.Statements[i];
+
+                if (blockParent.Statements.Count - 1 < i && !(blockParent.Statements[i + 1] is LocalDeclarationStatementSyntax))
+                    break;
+
+                var localDeclarationStatement = blockStatement as LocalDeclarationStatementSyntax;
+                if (localDeclarationStatement == null) break;
+
+                assignmentExpressions.Add(localDeclarationStatement);
+            }
+
+            return assignmentExpressions;
+        }
+    }
+}
---
+++ test/CodeCracker.Test/AssignSymbolAlignmentTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class AssignSymbolAlignmentTest : CodeFixVerifier
+    {
+        [Fact]
+        public void WhenThereAreOnlyOneAssignStatementNoAnalysisAreCalled()
+        {
+            const string source = @"var st = ""this is a string!"";";
+
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenNextLineIsBlanckNoAnalysisAreCalled()
+        {
+            string source =@"
+public void method()
+{
+    var st = ""this is a string!"";
+
+    Console.WriteLine(st);
+}";
+
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenNextStatementIsntAssignStatetemeNoAnalysisAreCalled()
+        {
+            string source = @"
+public void method()
+{
+    var st = ""this is a string!"";
+    Console.WriteLine(st.Length);
+}";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenNextStatementIsAAssignStatementAndAssignSignIsntAllignAnalysisAreCalled()
+        {
+            string source =
+@"public void method()
+{
+    var st = ""this is a string!"";
+    var st2 = ""this is another string!"";
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = AssignStatementAlignmentAnalyser.DiagnosticId,
+                Message = "Align assign symbol",
+                Severity = Microsoft.CodeAnalysis.DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 8) }
+            };
+
+            VerifyCSharpDiagnostic(source, expected);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AssignStatementAlignmentAnalyser();
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new AssignStatementAligmentCodeFixProvider();
+        }
+    }
+}
