ed63bfcb4f0c02175f85567cf0613382d579ab9c
Gerson Dias <gerson.dias@lambda3.com.br>
2014-11-17 15:40:58 -0200

    First working version of AssignStatement Analyser
    Need to do code fix yet.

--- src/CodeCracker/AssignStatementAlignmentAnalyser.cs
+++ src/CodeCracker/AssignStatementAlignmentAnalyser.cs
@@ -54,12 +54,21 @@ namespace CodeCracker
                     {
                         var previousStatement = parentBlockStatements.Value[i - 1];
                         if (previousStatement is LocalDeclarationStatementSyntax)
-                            return;
+                            break;
                     }
 
                     var nextStatement = parentBlockStatements.Value[i+1];
                     if (nextStatement is LocalDeclarationStatementSyntax)
-                        localDeclarationList.Add(nextStatement as LocalDeclarationStatementSyntax);
+                    {
+                        localDeclarationList.Add(currentStatement as LocalDeclarationStatementSyntax);
+                        for(int j = i+1; j < parentBlockStatements.Value.Count(); j++)
+                        {
+                            if (parentBlockStatements.Value[j] is LocalDeclarationStatementSyntax)
+                            {
+                                localDeclarationList.Add(parentBlockStatements.Value[j] as LocalDeclarationStatementSyntax);
+                            }
+                        }
+                    }
 
                     break;
                 }
@@ -80,112 +89,19 @@ namespace CodeCracker
             //var x = "teste";
             //var x2 = "teste";
 
-            var trivias = localDeclarationList.Select(x => x.DescendantTrivia());
-            var tokens = trivias.Where(x => x.All(t => t.Token.IsKind(SyntaxKind.EqualsToken)));
-            //var t = trivias.;
+            if (localDeclarationList.Count > 0)
+            {
+                var maxEqualsSymbolSpan = localDeclarationList.Select(x => x.GetText().ToString()).Max(t => t.IndexOf('='));
+                if (localDeclarationList.Any(x => x.GetText().ToString().IndexOf('=') != maxEqualsSymbolSpan))
+                    return true;
+            }
 
-
-             var maxSpanOfEqualsToken = localDeclarationList.Max(x => x.DescendantTrivia().Where(i => i.IsKind(SyntaxKind.EqualsToken)).Select(y => y.SpanStart));
-
-            return localDeclarationList.Any(x => x.DescendantTrivia().Any(i => i.IsKind(SyntaxKind.EqualsToken) && i.SpanStart < maxSpanOfEqualsToken.ElementAt(0)));
+            return false;
         }
 
         private void VariableDeclarationAnalyser(SyntaxNodeAnalysisContext context)
         {
             
         }
-
-        //private void Analyser(SyntaxNodeAnalysisContext context)
-        //{
-        //    var semanticModel = context.SemanticModel;
-        //    var statement = context.Node as VariableDeclarationSyntax;
-
-        //    var variableSymbol = semanticModel.GetSymbolInfo(statement).Symbol;
-        //    var assignmentExpressions = FindAssigmentExpressions(semanticModel, statement, variableSymbol);
-
-        //    if (assignmentExpressions.Count > 0)
-        //    {
-        //        var diagnostic = Diagnostic.Create(Rule, statement.GetLocation(), "Assign alignment.", "Align '=' symbols in assign declarations.");
-        //        context.ReportDiagnostic(diagnostic);
-        //    }
-        //}
-
-        //private List<VariableDeclarationSyntax> FindAssigmentExpressions(SemanticModel semanticModel, VariableDeclarationSyntax statement, ISymbol variableSymbol)
-        //{
-        //    var blockParent = statement.FirstAncestorOrSelf<BlockSyntax>();
-
-        //    if (blockParent == null) return null;
-
-        //    var assignmentExpressions = new List<VariableDeclarationSyntax>();
-
-        //    for (int i = 0; i < blockParent.Statements.Count; i++)
-        //    {
-        //        var currentStatement = blockParent.Statements[i];
-                
-        //        if (currentStatement != null && currentStatement.GetText() == statement.GetText())
-        //        {
-        //            assignmentExpressions.AddRange(CheckNearStatements(blockParent.Statements, i));
-        //        }
-        //    }
-
-
-
-
-        //    //var indexOfStatement = blockParent.Statements.Equals(statement);
-
-        //    ////Next statement is a LocalDeclaration?
-        //    //if (blockParent.Statements[indexOfStatement + 1] is VariableDeclarationSyntax)
-        //    //    return null;
-
-        //    //for (int i = indexOfStatement; i >= 0; i--)
-        //    //{
-
-
-
-
-
-        //    //    var blockStatement = blockParent.Statements[i];
-
-        //    //    if (blockParent.Statements.Count - 1 < i && !(blockParent.Statements[i + 1] is VariableDeclarationSyntax))
-        //    //        break;
-
-        //    //    var localDeclarationStatement = blockStatement as VariableDeclarationSyntax;
-        //    //    if (localDeclarationStatement == null) break;
-
-        //    //    assignmentExpressions.Add(localDeclarationStatement);
-        //    //}
-
-        //    return assignmentExpressions;
-        //}
-
-        //private List<VariableDeclarationSyntax> CheckNearStatements(SyntaxList<StatementSyntax> statements, int index)
-        //{
-        //    var localDeclarationStatements = new List<VariableDeclarationSyntax>();
-
-        //    if (!(statements[index] is VariableDeclarationSyntax))
-        //    {
-        //        return localDeclarationStatements;
-        //    }
-
-        //    //if statement is the last statement of code block or next statement isn't a LocalDeclarationStatement
-        //    //check if previous one is a LocalDeclarationStatement. If so, this need to be inserted in list
-        //    if (statements.Count - 1 == index && statements[index] is VariableDeclarationSyntax)
-        //    {
-        //        localDeclarationStatements.Add(statements[index] as VariableDeclarationSyntax);
-        //        localDeclarationStatements.AddRange(CheckNearStatements(statements, index - 1));
-        //    }
-        //    else if (!(statements[index + 1] is VariableDeclarationSyntax))
-        //    {
-        //        localDeclarationStatements.Add(statements[index] as VariableDeclarationSyntax);
-        //        localDeclarationStatements.AddRange(CheckNearStatements(statements, index - 1));
-        //    }
-        //    else if ((statements[index - 1] is VariableDeclarationSyntax))
-        //    {
-        //        localDeclarationStatements.Add(statements[index] as VariableDeclarationSyntax);
-        //        localDeclarationStatements.AddRange(CheckNearStatements(statements, index - 1));
-        //    }
-
-        //    return localDeclarationStatements;
-        //}
     }
 }
